6ca7a1030effb1f4f4e64ddb47ce2fd7fc66a522
P37R0L4 <lucao.gadget@gmail.com>
2021-11-03 15:44:33 -0300

    vercel config erros fixed

--- src/pages/theme.ts
+++ src/pages/theme.ts
@@ -6,5 +6,4 @@ const config = {
   useSystemColorMode: false,
 };
 // 3. extend the theme
-const theme = extendTheme({ config });
-export default theme;
+export const theme = extendTheme({ config });
