8fe8cba736634ffe53965163f217f51cd27da421
P37R0L4 <lucao.gadget@gmail.com>
2021-11-29 21:36:03 -0300

    Dont load on Mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import { AiFillApple } from 'react-icons/ai';
 
 export default function Index() {
   const [loading, setLoading] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
   const router = useRouter();
 
   const { setColorMode } = useColorMode();
@@ -17,32 +18,61 @@ export default function Index() {
   useEffect(() => {
     setColorMode('light');
 
-    setTimeout(() => {
-      loading <= 100
-        ? setLoading(loading + Math.floor(Math.random() * (20 - 1) + 1))
-        : setTimeout(() => {
-            router.push(`/home`);
-          }, 500);
-    }, 300);
+    !isMobile &&
+      setTimeout(() => {
+        loading <= 100
+          ? setLoading(loading + Math.floor(Math.random() * (20 - 1) + 1))
+          : setTimeout(() => {
+              router.push(`/home`);
+            }, 500);
+      }, 300);
   }, [loading]);
+
+  useEffect(() => {
+    detectMobile();
+  }, [isMobile]);
+
+  function detectMobile() {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      setIsMobile(true);
+    }
+  }
+
+  function LoadingPage() {
+    return (
+      <>
+        <Text textAlign="center">
+          Hello recruiter, alright!?...
+          <br /> My portfolio is loading...
+        </Text>
+
+        <Progress
+          w="full"
+          mb={10}
+          size="xs"
+          colorScheme="purple"
+          value={loading}
+        />
+      </>
+    );
+  }
 
   return (
     <Center w="full" h="100vh">
       <Fade in={loading >= 0 && loading <= 100}>
         <VStack spacing={10}>
           <AiFillApple size={60} />
-          <Text textAlign="center">
-            Hello recruiter, alright!?...
-            <br /> My portfolio is loading...
-          </Text>
-
-          <Progress
-            w="full"
-            mb={10}
-            size="xs"
-            colorScheme="purple"
-            value={loading}
-          />
+          {!isMobile ? (
+            <LoadingPage />
+          ) : (
+            <Text textAlign="center" px={10}>
+              Sorry, this page don't load on mobile... Have a game there!
+            </Text>
+          )}
         </VStack>
       </Fade>
     </Center>
